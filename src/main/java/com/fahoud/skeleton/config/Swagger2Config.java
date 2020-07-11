a73f3178dc39f2ac78605c0de0f4a055f03f8f91
Fahoud <daisypppppp@163.com>
2020-07-11 13:25:27 +0800

    新增Swagger2

---
+++ src/main/java/com/fahoud/skeleton/config/Swagger2Config.java
@@ -0,0 +1,106 @@
+package com.fahoud.skeleton.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* @author:Fahoud
+ * \* Date: 2020/4/26
+ * \* Time: 16:04
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:localhost:8080/swagger-ui.html
+ * 注意：Swagger对生成API文档的范围有三种不同的选择
+ *
+ * 生成指定包下面的类的API文档
+ * 生成有指定注解的类的API文档
+ * 生成有指定注解的方法的API文档
+ *
+ * 常用注解
+ * @Api：用于修饰Controller类，生成Controller相关文档信息
+ * @ApiOperation：用于修饰Controller类中的方法，生成接口方法相关文档信息
+ * @ApiParam：用于修饰接口中的参数，生成接口参数相关文档信息
+ * @ApiModelProperty：用于修饰实体类的属性，当实体类是请求参数或返回结果时，直接生成相关文档信息
+ * \
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config
+{
+    @Bean
+    public Docket createRestApi()
+    {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下控制器生成api文档
+                .apis(RequestHandlerSelectors.basePackage("com.xxx.xxx.controller"))
+                //为有@Api注解的控制器方法生成API文档
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any()).build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Mall")
+                .description("mall")
+                .contact("fahoud")
+                .version("1.0")
+                .build();
+    }
+
+    private List<SecurityContext> securityContexts()
+    {
+        //设置需要登录认证的路径
+        List<SecurityContext> result = new ArrayList<>();
+        result.add(getContextByPath("/brand/.*"));
+        return result;
+    }
+
+    private List<ApiKey> securitySchemes()
+    {
+        //设置请求头信息
+        List<ApiKey> result = new ArrayList<>();
+        ApiKey apiKey = new ApiKey("Authorization","Authorization","header");
+        result.add(apiKey);
+        return result;
+    }
+
+    private SecurityContext getContextByPath(String pathRegex)
+    {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(pathRegex))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth()
+    {
+        List<SecurityReference> result = new ArrayList<>();
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+                "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        result.add(new SecurityReference("Authorization", authorizationScopes));
+        return result;
+    }
+}
